01d2755117aec49d1a15b379c4005181bda6a487
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-07-09 21:29:11 +0530

    Adding CRUD_create-14 main file

---
+++ CRUD_create-14.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI , Depends , HTTPException
+from sqlalchemy import create_engine , Column , Integer , String
+from sqlalchemy.orm import Session , sessionmaker , declarative_base
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///./CRUD_create_test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread":False}
+)
+SessionLocal = sessionmaker(bind=engine)
+
+Base=declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer , primary_key=True , index=True)
+    title = Column(String)
+    completed = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/todo")
+def create_todo(title:str , db : Session = Depends(get_db)):
+    todo = Todo(title=title , completed="False")
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return{
+        "message":"Todo Created!",
+        "data":todo
+    }
