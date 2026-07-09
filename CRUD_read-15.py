b3de617e16957a665f5fe0a7320741b9acfbd0c4
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-07-09 23:12:49 +0530

    Add CRUD read implementation

---
+++ CRUD_read-15.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI , Depends , HTTPException
+from sqlalchemy import create_engine , Column , Integer , String 
+from sqlalchemy.orm import Session , sessionmaker , declarative_base
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///./CRUD_read_test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread":False}
+)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer , primary_key=True , index=True)
+    title = Column(String)
+    completed = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/todos")
+def create_todo(title:str , db : Session = Depends(get_db)):
+    todo=Todo(title=title , completed="False")
+
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return {
+        "message":"Todo created!",
+        "data":todo
+    }
+
+@app.get("/todos")
+def get_todos(db:Session = Depends(get_db)):
+    todos = db.query(Todo).all()
+
+    return{
+        "Total":len(todos),
+        "data":todos
+    }
+
+@app.get("/todo/{todo_id}")
+def get_todo(todo_id:int , db:Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo :
+        raise HTTPException(status_code=404 , detail="Todo not found")
+    return todo
