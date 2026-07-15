1aa31861f23af0ee953c1d57b17316213e4f6a6f
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-07-15 22:03:45 +0530

    Updating better structure

--- Async-18.py
+++ Async-18.py
@@ -4,12 +4,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
+
+@app.get("/sync")
+def sync_endpoint():
+    time.sleep(3)
+    return {"message": "Sync API"}
+
+@app.get("/async")
+async def async_endpoint():
     await asyncio.sleep(3)
-    return {
-        "message":"Async API"
-    }
+    return {"message": "Async API"}
+
 
 # Sync structure 
 # def task():
