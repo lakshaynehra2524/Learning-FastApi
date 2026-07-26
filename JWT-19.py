a8cab0bb4e7e0a41a7a1d57ea1ed3918a06ea189
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-07-26 14:16:03 +0530

    creating token

--- JWT-19.py
+++ JWT-19.py
@@ -7,3 +7,13 @@ app = FastAPI()
 SECRET_KEY = "mysecret"
 ALGORITHM = "HS256"
 
+def create_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+    to_encode.update({
+        "exp":expire
+    })
+    token = jwt.encode(to_encode , SECRET_KEY , algorithm=ALGORITHM)
+
+    return token
+
