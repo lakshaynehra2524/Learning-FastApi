7c26b7b1ad593abf128a553e558b887ad7a5016b
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-07-27 12:24:57 +0530

    JWT Config

---
+++ OAuth2-20.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI , Depends , HTTPException
+from jose import jwt
+from fastapi.security import OAuth2PasswordBearer , OAuth2PasswordRequestForm
+from datetime import datetime , timedelta , timezone
+from passlib.context import CryptContext
+
+app = FastAPI()
+
+# JWT config 
+SECRET_KEY = "mysecret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 
+
+#password hashing setup 
