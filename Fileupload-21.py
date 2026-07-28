24293973c84b796da044ca2f14fdfdc902b20afa
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-07-28 22:59:40 +0530

    step 1

--- Fileupload-21.py
+++ Fileupload-21.py
@@ -2,3 +2,9 @@ from fastapi import FastAPI , UploadFile , File , HTTPException
 from fastapi.staticfiles import StaticFiles
 import os
 import shutil
+
+app = FastAPI()
+
+#Step-1: Ensure uploads folder exist
+
+UPLOAD_DIR = "uploads"
