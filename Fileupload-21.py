0913f6eb19346fd275a1d16cfa12920392ffdd69
Lakshay <lakshaynehra2524@gmail.com>
2026-08-02 23:08:26 +0530

    Add API to retrieve uploaded file URL

--- Fileupload-21.py
+++ Fileupload-21.py
@@ -40,3 +40,14 @@ def home():
     return{
         "message":"File Uploaded api Running"
     }
+#Step-4:Get File URL API
+@app.get("/files/{filename}")
+def get_file(filename:str):
+    file_path = os.path.join(UPLOAD_DIR,filename)
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+    
+    return{
+        "file_url":f"http://127.0.0.1:8000/files/{filename}"
+    }
