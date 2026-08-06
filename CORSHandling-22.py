a4773f59d17b5e8c0efd2233d6b95b209706ba73
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-06 18:40:22 +0530

    Setting u middleware with origin

--- CORSHandling-22.py
+++ CORSHandling-22.py
@@ -2,3 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+#Allowed Origins(Front-end URl)
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins, #allowed FE
+    allow_credentials = True,
+    allow_methods = ["*"], #GET,PUT,POST,DELETE
+    allow_headers=["*"]
+)
