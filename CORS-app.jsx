280e385ccfcad520b2a19f634c271bff6da9891a
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-06 18:55:31 +0530

    Updating react file

--- CORS-app.jsx
+++ CORS-app.jsx
@@ -1,7 +1,20 @@
-export default function APP(){
+import React , {useState , useEffect} from 'react' ;
+
+const App = () => {
+    const [data , setData] = useState(null);
+
     return (
-        <dev className="App">
-            <h1>React App</h1>
-        </dev>
-    );
+        <div style={{padding:"20px"}}>
+            <h1>API DATA</h1>
+            {
+                data ? (
+                    <p>Message : {data.message} </p>
+                ) : (
+                    <p>
+                        Loading...
+                    </p>
+                )
+            }
+        </div>
+    )
 }
