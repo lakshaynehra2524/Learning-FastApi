d07503b8035c15672073726dd01d7f02dcefd27e
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-07 14:42:38 +0530

    Loading .env variables

--- Env_var-23.py
+++ Env_var-23.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
+from dotenv import load_dotenv
 app = FastAPI()
 
+load_dotenv()
 
+origins = os.getenv("ORIGINS")
+SECRET_KEY = os.getenv("SECRET_KEY")
+DB_URL = os.getenv("DB_URL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ORIGINS, #allowed FE
+    allow_origins = origins, #allowed FE
     allow_credentials = True,
     allow_methods = ["*"], #GET,PUT,POST,DELETE
     allow_headers=["*"]
