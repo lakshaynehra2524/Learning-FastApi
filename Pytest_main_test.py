09019c325e6ad211540468afe20c1947f3a8269e
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-08 18:15:23 +0530

    Adding test api for home

--- Pytest_main_test.py
+++ Pytest_main_test.py
@@ -1,7 +1,13 @@
 from fastapi.testclient import TestClient
-from Pytest_main_test import app
 
+from Pytest_main_24 import app 
 
 client = TestClient(app)
 
 #Test Home API
+def test_home():
+    response = client.get("/")
+    # Status code check 
+    assert response.status_code == 200 
+    # Response data check 
+    assert response.json() == {"message":"Hello Lakshay"}
