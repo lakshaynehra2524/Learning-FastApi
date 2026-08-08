2e68b644eda25d290927eb40ecb1883f29a7042a
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-08 18:18:16 +0530

    Adding test api for Add

--- Pytest_main_test.py
+++ Pytest_main_test.py
@@ -11,3 +11,10 @@ def test_home():
     assert response.status_code == 200 
     # Response data check 
     assert response.json() == {"message":"Hello Lakshay"}
+
+# Test Add API 
+def test_add():
+    response = client.get("/add?a=5&b=3")
+
+    assert response.status_code == 200
+    assert response.json() == {"result": 8}
