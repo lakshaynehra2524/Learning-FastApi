c4beaf85c40e0c667803fd7c4ed27e89b6422eb1
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-11 17:29:44 +0530

    Getting all posts from third party API

--- Thirdparty_API-25.py
+++ Thirdparty_API-25.py
@@ -2,3 +2,13 @@ import requests
 
 from fastapi import FastAPI , HTTPException
 
+app = FastAPI()
+
+# GET all data 
+@app.get("/posts")
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    return response.json()
+
+
