aa8ab187ccd38185c3a4e2265c8dcbcda11e5ac2
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-12 15:51:41 +0530

    Adding libraries in Fastapi style

--- Webcrawling-26.py
+++ Webcrawling-26.py
@@ -1,9 +1,15 @@
-import requests 
+# import requests 
+# from bs4 import BeautifulSoup
+
+# url = "http://example.com"
+
+# response = requests.get(url)
+
+# soup = BeautifulSoup(requests.text , "html.parse")
+# print(soup.title.text)
+
+from fastapi import FastAPI
+import requests
 from bs4 import BeautifulSoup
 
-url = "http://example.com"
-
-response = requests.get(url)
-
-soup = BeautifulSoup(requests.text , "html.parse")
-print(soup.title.text)
+app = FastAPI()
