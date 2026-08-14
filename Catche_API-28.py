792d086dc15b6510c913e1fb730c9d813a124869
Lakshaynehra2524 <lakshaynehra2524@gmail.com>
2026-08-14 14:32:18 +0530

    Final Catche file

--- Catche_API-28.py
+++ Catche_API-28.py
@@ -5,11 +5,10 @@ import time
 
 app = FastAPI()
 
-# Cache Storage
-cache_data =[]
+#Cache Storage
+cache_data = []
 last_fetch = 0
 
-# Getting news 
 @app.get("/news")
 def get_news():
     global cache_data, last_fetch
@@ -29,16 +28,19 @@ def get_news():
             item.text for item in soup.find_all("span", class_="titleline")
         ]
 
+
         last_fetch = time.time()
 
     else:
-        print("Using catche data")
+        print("Using catche Data")
 
-        end = time.time()
+    end = time.time()
 
-        time_taken = round(end - start ,4)
+    time_taken = round(end-start,4)
 
-        return {
-            "time_taken" : time_taken ,
-            "data" : cache_data[:5]
-        }
+    print("Time Taken: ", time_taken)
+
+    return{
+        "time_taken":time_taken,
+        "data":cache_data[:5]
+    }
